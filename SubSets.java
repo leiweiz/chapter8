77909d779b14af6c758e2e695595ffd9d9c5701a
leiweiz <zhengleiwei@hotmail.com>
2014-07-28 00:27:24 -0400

    q4 2st solution

--- SubSets.java
+++ SubSets.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
 public class SubSets{
-
+    // my solution
     public static void printSet(ArrayList<Integer> set){
         System.out.print("{ ");
         for (Integer i : set){
@@ -34,11 +34,44 @@ public class SubSets{
         showSubsets(newset, newSubset2);
     }
 
+
+    //refer solution
+    public static ArrayList<ArrayList<Integer>> getSubsets(ArrayList<Integer> set, int index){
+        ArrayList<ArrayList<Integer>> allsubsets;
+        if (set.size()==index){
+            allsubsets = new ArrayList<ArrayList<Integer>>();
+            allsubsets.add(new ArrayList<Integer>());
+        }else{
+            allsubsets = getSubsets(set, index+1);
+            int item = set.get(index);
+            ArrayList<ArrayList<Integer>> moresubsets = 
+                   new ArrayList<ArrayList<Integer>>();
+            for (ArrayList<Integer> subset : allsubsets){
+                ArrayList<Integer> newsubset = new ArrayList<Integer>();
+                newsubset.addAll(subset);
+                newsubset.add(item);
+                moresubsets.add(newsubset);
+            }
+            allsubsets.addAll(moresubsets);
+        }
+        return allsubsets;
+    }
+
     public static void main(String[] args){
+        ArrayList<ArrayList<Integer>> allset = new ArrayList<ArrayList<Integer>>();
         ArrayList<Integer> set = new ArrayList<Integer>();
         ArrayList<Integer> subset = new ArrayList<Integer>();
         set.add(1);
         set.add(2);
-        SubSets.showSubsets(set, subset);
+        set.add(3);
+        //SubSets.showSubsets(set, subset);
+        allset = SubSets.getSubsets(set, 0);
+        for(ArrayList<Integer> sub : allset){
+            System.out.print("{ ");
+            for (Integer i : sub){
+                System.out.print(i + " " );
+            }
+            System.out.println("}");
+        }
     }
 }
